a0f3c868344498920d3b659549a5c78ad9300b43
Anjali <anjalichakravarty821@gmail.com>
2024-07-06 11:14:26 +0530

    my java

---
+++ VariableDemo.java
@@ -0,0 +1,27 @@
+//Write a program to display marks, age , section of a student.//
+
+class VariableDemo{
+    public static void main(String[] args){
+        //declare variable
+        //float or double
+        double marks;
+        int age;
+        char section;
+        //initialise the variables
+        //identifier = value ot literal;
+        marks = 40.5;
+        age = 19;
+        section = 'C';
+
+        //declare and initialise an string variable
+        String name  = "Anjali"
+        //printing variable values on console
+
+        System.out.println("Marks = " + marks + "\");
+        System.out.println("Age = " + age  + "\");
+        System.out.println("Section = " + section + "\");
+        System.out.println(name);
+    }
+}
+
+// wap to display name emal and phonenumber on console//..
