39638cb096f2ae27b632d5870502ae08412fce7f
Anjali <anjalichakravarty821@gmail.com>
2024-09-06 20:14:59 +0530

    todays java file

---
+++ ImportantMethodOfString.java
@@ -0,0 +1,12 @@
+public class ImportantMethodOfString{
+    public static void main(String[] args){
+        string input = "Anjali Prajapati"
+        int length = input.length();
+        //getting character from string using charAt
+        char firstChar = input.charAt(0);
+        //getting index of character from string.
+        // will return the first occured index if the character is present in the string.
+        // or else will return -1
+        
+    }
+}
